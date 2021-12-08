c1636429f8b151e7d06333ec22dc57120e602b8b
Rahul Sharma <rahulat4200@gmail.com>
2021-12-08 11:38:47 +0530

    Step 6 completed. Design and build the ProductScreen component done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       >
         <Routes>
           <Route path='/' element={<HomeScreen />} />
+          <Route path='/product/:id' element={<ProductScreen />} />
         </Routes>
       </Flex>
       <Footer />
--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -4,26 +4,29 @@ import { IoStar, IoStarOutline, IoStarHalf } from 'react-icons/io5';
 const Rating = ({ value, text, color = 'red.500' }) => {
   return (
     <Flex alignItems='flex-start'>
-      <Icon
-        color={color}
-        as={value >= 1 ? IoStar : value >= 0.5 ? IoStarHalf : IoStarOutline}
-      />
-      <Icon
-        color={color}
-        as={value >= 2 ? IoStar : value >= 1.5 ? IoStarHalf : IoStarOutline}
-      />
-      <Icon
-        color={color}
-        as={value >= 3 ? IoStar : value >= 2.5 ? IoStarHalf : IoStarOutline}
-      />
-      <Icon
-        color={color}
-        as={value >= 4 ? IoStar : value >= 3.5 ? IoStarHalf : IoStarOutline}
-      />
-      <Icon
-        color={color}
-        as={value >= 5 ? IoStar : value >= 4.5 ? IoStarHalf : IoStarOutline}
-      />
+      <Box mr='2'>
+        <Icon
+          color={color}
+          as={value >= 1 ? IoStar : value >= 0.5 ? IoStarHalf : IoStarOutline}
+        />
+        <Icon
+          color={color}
+          as={value >= 2 ? IoStar : value >= 1.5 ? IoStarHalf : IoStarOutline}
+        />
+        <Icon
+          color={color}
+          as={value >= 3 ? IoStar : value >= 2.5 ? IoStarHalf : IoStarOutline}
+        />
+        <Icon
+          color={color}
+          as={value >= 4 ? IoStar : value >= 3.5 ? IoStarHalf : IoStarOutline}
+        />
+        <Icon
+          color={color}
+          as={value >= 5 ? IoStar : value >= 4.5 ? IoStarHalf : IoStarOutline}
+        />
+      </Box>
+      <Text>{text && text}</Text>
     </Flex>
   );
 };
