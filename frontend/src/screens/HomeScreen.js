a4c0e0a02dad20ff1c53f983a2d0eb16ffc0c1cf
Rahul Sharma <rahulat4200@gmail.com>
2021-12-14 17:15:20 +0530

    Step 22 and 23 completed. Create our first reducer and Getting Redux State in the Home Screen done.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,19 +1,17 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Heading, Grid } from '@chakra-ui/react';
 import Product from '../components/Product';
-// import products from '../products';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, error } = productList;
 
   useEffect(() => {
-    const fetchProductds = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-
-    fetchProductds();
+    dispatch(listProducts());
   }, []);
 
   return (
@@ -21,11 +19,17 @@ const HomeScreen = () => {
       <Heading as='h2' mb='8' fontSize='3xl'>
         Latest Products
       </Heading>
-      <Grid templateColumns='repeat(4, 1fr)' gap='8'>
-        {products.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
-      </Grid>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>ERRROR!</p>
+      ) : (
+        <Grid templateColumns='repeat(4, 1fr)' gap='8'>
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </Grid>
+      )}
     </>
   );
 };
