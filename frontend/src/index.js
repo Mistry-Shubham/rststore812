ba36271cd81d17d03909c6b495fd320e1a7b5ccc
Rahul Sharma <rahulat4200@gmail.com>
2021-12-14 16:16:03 +0530

    Step 21 completed. Introduction to Redux done.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
+import store from './store';
 import App from './App';
 
 render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
+  <Provider store={store}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Provider>,
   document.querySelector('#root')
 );
